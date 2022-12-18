accc0bf078f989dc9468c3e1368df3380b112761
Freider Montano <freymanuser@gmail.com>
2022-12-17 23:40:52 -0300

    ajuste de incremento y decremento en main.js

--- js/main.js
+++ js/main.js
@@ -1,84 +1,84 @@
 let shop = document.getElementById('shop');
 
 let shopItemData = [{
-    id: 'dfgh',
-    name: 'ropa casual verano',
+    id: 'dfghtn',
+    name: 'ropa  verano',
     price: 18990,
-    desc: 'polera casual',
+    desc: 'polera algodón',
     img: 'images/img2.jpg'
 },
 {
-    id: 'dfgh',
-    name: 'jordan polera white',
+    id: 'dfghkb',
+    name: 'Polera Tribe',
     price: 17990,
-    desc: 'polera casual',
+    desc: ' poliéster de algodón',
     img: 'images/img4.jpg'
 },
 {
-    id: 'dfgh',
+    id: 'dfghmytg',
     name: 'ropa casual verano',
     price: 19990,
     desc: 'polera casual',
     img: 'images/img1.jpg'
 },
 {
-    id: 'dfgh',
+    id: 'dfghdcf',
     name: 'ropa casual verano',
     price: 13990,
     desc: 'polera casual',
     img: 'images/img3.jpg'
 },
 {
-    id: 'dfgh',
-    name: 'jordan polera white',
+    id: 'dfghyuo',
+    name: 'Set de Accesorios ',
     price: 21990,
-    desc: 'polera casual',
+    desc: 'Increíble set belleza',
     img: 'images/img5.jpg'
 },
 {
-    id: 'dfgh',
-    name: 'jordan polera white',
-    price: 44990,
-    desc: 'polera casual',
+    id: 'dfghdft',
+    name: 'Audífonos Earbuds',
+    price: 7990,
+    desc: 'Compatible con Android',
     img: 'images/img7.jpg'
 },
 {
-    id: 'dfgh',
+    id: 'dfghop',
     name: 'Silla Negra/Natural',
     price: 14990,
     desc: 'Para que tu decisión sea la más certera',
     img: '/images/img14.jpg'
 },
 {
-    id: 'dfgh',
+    id: 'dfghml',
     name: 'Consola Nintendo Switch',
     price: 419990,
     desc: 'modelo portátil para llevar',
     img: '/images/img13.jpg'
 },
 {
-    id: 'dfgh',
+    id: 'dfghbvs',
     name: 'Polera Manga Corta',
     price: 12990,
     desc: 'Polera Mujer Manga Corta Estampada',
     img: '/images/img11.jpg'
 },
 {
-    id: 'dfgh',
+    id: 'dfghvty',
     name: 'Smartphone Motorola',
     price: 44990,
     desc: 'Sistema Operativo: Android',
     img: '/images/img9.jpeg'
 },
 {
-    id: 'dfgh',
+    id: 'dfghcf',
     name: 'Monitor Gamer 32 Qhd',
     price: 44990,
     desc: 'monitor LG QHD ultra dinamico',
     img: '/images/img10.jpg'
 },
 {
-    id: 'dfgh',
+    id: 'dfghte',
     name: 'Sony playstation 4',
     price: 44990,
     desc: ' consola PlayStation',
@@ -86,15 +86,21 @@ let shopItemData = [{
 },
 ];
 
+
+// listado de compra por cada producto que se almacenara
+let listaDeProduct = [];
+
 let generateShop = () => {
     return (shop.innerHTML = shopItemData.map((x) => {
+        // id productos
+        let {id, name, price, desc, img} = x;
         return `
-        <div class="col-md-4 col-lg-3  col-sm-6">
+        <div class="col-md-4 col-lg-3  col-sm-6 id=product-id${id}">
                     <div class="card  mb-5">
-                        <img src="${x.img}" alt="producto" class="card-img-top">
+                        <img src="${img}" alt="producto" class="card-img-top">
                         <div class="card-body">
-                            <h5 class="title text-center">${x.name}</h5>
-                            <p class="text-center">${x.desc}</p>
+                            <h5 class="title text-center">${name}</h5>
+                            <p class="text-center">${desc}</p>
                             <div class="start text-center">
                                 <i class="fa-solid fa-star"></i>
                                 <i class="fa-solid fa-star"></i>
@@ -103,11 +109,11 @@ let generateShop = () => {
                             </div>
     
                             <div class="price-quantity d-flex justify-content-around mt-4">
-                                <span class="text-center d-block">$${x.price}</span>
+                                <span class="text-center d-block">$${price}</span>
                                 <div class="buttons g-5 d-flex flex-row align-items-center">
-                                    <i class="fa-solid fa-plus"></i>
-                                    <div class="quantity">0</div>
-                                    <i class="fa-solid fa-minus"></i>
+                                    <i  onclick="increment(${id})" class="fa-solid fa-plus"></i>
+                                    <div id=${id} class="quantity">0</div>
+                                    <i onclick="decrement(${id})" class="fa-solid fa-minus"></i>
                                 </div>
                             </div>
                         </div>
@@ -119,3 +125,41 @@ let generateShop = () => {
 
 generateShop();
 
+
+
+// funciones para el valor incrementar y decrementar
+
+let increment = (id) =>{
+    let selectedItem = id;
+    let search = listaDeProduct.find((x)=> x.id === selectedItem.id);
+
+    if(search === undefined){
+        listaDeProduct.push({
+        id : selectedItem.id,
+        item: 1,
+    });
+        
+    }else{
+       search.item +=1 ;
+    }
+
+    console.log(listaDeProduct);
+    update(selectedItem.id);
+};
+let decrement = (id) =>{
+    let selectedItem = id;
+    let search = listaDeProduct.find((x)=> x.id === selectedItem.id);
+
+    if(search.item === 0) return;//detengo el proceso para no obtener -1 en el decremento
+        else{
+       search.item -=1 ;
+    }
+    console.log(listaDeProduct);
+    update(selectedItem.id);
+
+};
+let update = (id) =>{
+    let search = listaDeProduct.find((x)=> x.id === id);
+console.log(search.item);
+document.getElementById(id).innerHTML = search.item;
+};
